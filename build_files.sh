a82a50e2723cae77d8e36c7af954608cf8680784
Milksheyke <amilksheyke@gmail.com>
2024-02-18 18:05:05 -0500

    Remove vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# !! INSTALL NVM AND NPM !!
 pip3 install django
 pip3 install django-compressor
 pip3 install django-tailwind
+pip install gunicorn
 python3 manage.py collectstatic
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
