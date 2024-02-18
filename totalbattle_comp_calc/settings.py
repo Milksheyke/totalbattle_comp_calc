1062d398d5d9de93e8db36588908f041a382ff0c
Milksheyke <amilksheyke@gmail.com>
2024-02-18 15:33:13 -0500

    Remove sqlite3 config

--- totalbattle_comp_calc/settings.py
+++ totalbattle_comp_calc/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = "totalbattle_comp_calc.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
