baf4a5095a5516ad05a3019124e64503010789a4
Milksheyke <amilksheyke@gmail.com>
2024-02-18 23:29:45 -0500

    fsa

--- totalbattle_comp_calc/settings.py
+++ totalbattle_comp_calc/settings.py
@@ -141,22 +141,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": "/root/django_errors.log",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-    },
-}
